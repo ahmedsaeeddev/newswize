c9ef59c6abdce65fc8e313197f646bed5211ba85
Ahmed Saeed <freelancerahmeddev@gmail.com>
2024-08-02 15:57:15 +0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 let userInput = 'tech';
 let news = document.querySelector('.news');
 const fetchNews = () => {
-
-    fetch(`https://newsapi.org/v2/everything?q=${userInput}&apiKey=a47fb4a80e6d417c93d94c292b31b633`)
-        .then((res) => res.json())
-        .then((json) => {
-            console.log(json);
-            let containedArray = json.articles.filter((articles) => articles.urlToImage !== null)
-            for (let i = 0; i < containedArray.length; i++) {
-                news.innerHTML += `
+  fetch(`https://newsapi.org/v2/everything?q=${userInput}&apiKey=a47fb4a80e6d417c93d94c292b31b633`)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      let containedArray = json.articles.filter((articles) => articles.urlToImage !== null)
+      for (let i = 0; i < containedArray.length; i++) {
+        news.innerHTML += `
         <div class="card" style="width: 18rem;">
         <img class="card-img-top" src="${containedArray[i].urlToImage}">
         <div class="card-body">
@@ -17,11 +16,8 @@ const fetchNews = () => {
           <a href="${containedArray[i].url}" target='_blank' class="btn btn-primary">Read More</a>
         </div>
       `
-
-
-
-            }
-        })
+      }
+    } )
 
 
 
